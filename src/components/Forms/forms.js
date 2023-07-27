26ecd8ec55ba321d304f7fd69d92723a7e5259c7
Leonardo Silva Ferreira <leonardosilvaferreira@hotmail.com>
2023-07-27 08:58:35 -0300

    Add Form LoginBox

--- src/components/Forms/forms.js
+++ src/components/Forms/forms.js
@@ -1,4 +1,6 @@
-import styles from './forms.module.css';
+import stylesForms from './forms.module.css'
+import stylesFormDados from './formDados.module.css';
+import FormUser from './formUser.module.css';
 
 function Form(){
 
@@ -9,18 +11,35 @@ function Form(){
 
     return(
         <>
-        <section className={styles.Forms}>
-            <form onSubmit={cadastrarDados} className={styles.FormUser}>
-                <label className={styles.Lb} htmlFor="name">Nome:</label>
-                    <input name="nome" id='nome' type='text' className={styles.Infos} placeholder='Nome Completo'/>
+        <section className={stylesForms.Forms}>
+            <form onSubmit={cadastrarDados} className={stylesFormDados.FormDados}>
+                
+                <label className={stylesFormDados.Lb} htmlFor="name">Nome:</label>
+                    <input name="nome" id='nome' type='text' className={stylesFormDados.Infos} placeholder='Nome Completo'/>
 
-                    <label htmlFor='email' className={styles.Lb}>Email:</label>
-                    <input htmlFor='email' id='email' type='email' className={styles.Infos} placeholder='seuemail@domínio.com.br'/>
+                    <label htmlFor='email' className={stylesFormDados.Lb}>Email:</label>
+                    <input htmlFor='email' id='email' type='email' className={stylesFormDados.Infos} placeholder='seuemail@domínio.com.br'/>
 
-                    <button id={styles.Send} type='submit'>Enviar</button>
-                    <button id={styles.Clear} type='reset'>Limpar</button>
+                    <button id={stylesFormDados.Send} type='submit'>Enviar</button>
+                    <button id={stylesFormDados.Clear} type='reset'>Limpar</button>
 
             </form>
+
+            <section className={FormUser.LoginBox}>
+                <form className={FormUser.FormUser}>
+                    <div className={FormUser.UserBox}>
+                        <label htmlFor='name'></label>
+                        <input type='text' id='name' name='name' placeholder='Usuário' required=''></input>
+                    </div>
+                    <div className={FormUser.UserBox}>
+                        <label htmlFor='password'></label>
+                        <input type='password' placeholder='Senha' required=''></input>
+                    </div><center>
+                    <a href='#'>SEND
+                        <span></span>
+                    </a></center>
+                </form>
+            </section>
         </section>
         </>
     )

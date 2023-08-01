8053972ad5e3cc13faddab663329d0d6078d8273
Leonardo Silva Ferreira <leonardosilvaferreira@hotmail.com>
2023-08-01 12:39:32 -0300

    Use of Emotion

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,4 +1,4 @@
-import styles from '../Footer/footer.module.css';
+import { Content, GitHubIcon } from './styles';
 
 function Footer(){
 
@@ -8,12 +8,13 @@ function Footer(){
 
     return (
         <>
-        <footer className={styles.Footer}>
-            <p className=''>Webpage created by {creator.toUpperCase()}</p>
+        <Content>
+            <p>Webpage created by {creator.toUpperCase()}</p>
             <h6>{emailCreator.toLowerCase()}</h6>
-            <a className={styles.GitHubIcon} href='https://github.com/leozinrd' target='blank'>
-                <img className={styles.WhatsappImage} src={urlGitHubImage} alt="GitHub"/></a>
-      </footer>
+            <GitHubIcon href='https://github.com/leozinrd' target='blank'>
+                <img src={urlGitHubImage} alt="GitHub"/>
+            </GitHubIcon>
+      </Content>
         </>
     )
 
